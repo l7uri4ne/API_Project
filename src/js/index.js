b35e0ecfaba126b2ec437bb3b8c8b42c2c4889ca
Lauriane Samuel <l7uri4ne@gmail.com>
2022-03-16 14:02:58 +0100

    Add frontPage.js to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,42 @@
 import '../css/app.scss';
 import Characters from './characters';
+import Character from './character';
+import FrontPage from "./frontPage";
 
 
 class App {
     constructor() {
+        this.initEls();
         this.initApp();
     }
 
-    initApp() {
+    initEls() {
+
+        this.els = {
+            characters: document.querySelector('.js-characters'),
+            character: document.querySelector('.js-character'),
+            front: document.querySelector('.js-frontPage')
+        }
+
+
+        //new Character();
+    }
+
+    initApp(){
+
         // Start application
-        new Characters();
+        if (this.els.characters){
+            new Characters();
+        }
+
+        if (this.els.character){
+            new Character();
+        }
+
+        if (this.els.front){
+            new FrontPage();
+        }
+
     }
 }
 
