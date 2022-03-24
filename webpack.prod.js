3aed7db6d46bad2795254525678820eda1af6b93
Lauriane Samuel <l7uri4ne@gmail.com>
2022-03-24 11:24:30 +0100

    Compilation build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,60 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 config = {
     entry: './src/js/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '',
     },
-    mode: 'production'
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.html/,
+                loader: 'html-loader',
+
+            },
+            {
+                test: /\.hbs/,
+                loader: 'handlebars-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+            },
+            { test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: { filename: 'images/[name][ext]' }
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)$/,
+                type: 'asset/resource',
+                generator: { filename: 'fonts/[name][ext]' }
+            },
+        ]},
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/characters.html',
+            filename: 'characters.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/character.html',
+            filename: 'character.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: `style.css`
+        })
+    ],
 };
 module.exports = config;
